5e93d8985f9874bdf701f58745ceac18911614ba
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-19 16:31:59 -0300

    mejora

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,23 @@ fetch('https://swapi.co/api/people/')
     })
     .then(function (data) {
         console.log(data);
+        let containerImages = document.querySelector('.containerImages');
 
         for (let index = 0; index < data.results.length; index++) {
-            let containerImages = document.querySelector('.containerImages');
+            if (data.results[index].name === swapi[index].name) {
             let divImage = document.createElement('div');
+            divImage.className = 'divImage';
+            let image = document.createElement('img');
+            image.setAttribute('src', swapi[index].image);
             let textImage = document.createTextNode(data.results[index].name);
             let ptextImage = document.createElement('p');
             ptextImage.appendChild(textImage);
+            divImage.appendChild(image);
             divImage.appendChild(ptextImage);
             containerImages.appendChild(divImage);
+            }
         }
     });
+
+
+    
---
+++ assets/js/data.js
@@ -0,0 +1,42 @@
+var swapi = [{
+    "name": "Luke Skywalker",
+    "image": "assets/images/luke-skywalker.png"
+  },
+  {
+    "name": "C-3PO",
+    "image": "assets/images/c3p0.png"
+  },
+  {
+    "name": "R2-D2",
+    "image": "assets/images/r2d2.png"
+  },
+  {
+    "name": "Darth Vader",
+    "image": "assets/images/darth-vader.png"
+  },
+  {
+    "name": "Leia Organa",
+    "image": "assets/images/princess-leia.png"
+  },
+  {
+    "name": "Owen Lars",
+    "image": "assets/images/stormtrooper_icon-icons.com_76962.png"
+  },
+  {
+    "name": "Beru Whitesun lars",
+    "image": "assets/images/BeruWhitesunlars.png"
+  },
+  {
+    "name": "R5-D4",
+    "image": "assets/images/r5-d4.jpg"
+  },
+  {
+    "name": "Biggs Darklighter",
+    "image": "assets/images/biggsDarklighter.png"
+  },
+  {
+    "name": "Obi-Wan Kenobi",
+    "image": "assets/images/Obi-Wan Kenobi.png"
+  },
+  
+];
